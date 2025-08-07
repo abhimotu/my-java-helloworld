c24cd49582902f333dbc72bd04b06ee29490b2b2
abhimotu <abhi87_mcc@live.com>
2025-08-07 16:29:16 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
-public class HelloWorldParam {
+public class HelloWorld {
   public static void main(String[] args) {
     if (args.length == 0) {
       System.out.println("Hello world");
     } else {
-      String name = args
+      String name = args;
       System.out.println("Hello " + name);
     }
   }
